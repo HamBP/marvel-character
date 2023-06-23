b95c394c698e36d5059ac467088824ee5d237245
algosketch <sjy_9484@naver.com>
2023-06-23 21:35:08 +0900

    Feat: 바텀 네비게이션 NavHost 구현

---
+++ app/src/main/java/me/algosketch/shopliveassignment/presentation/navigation/SlNavHost.kt
@@ -0,0 +1,28 @@
+package me.algosketch.shopliveassignment.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import me.algosketch.shopliveassignment.presentation.favorite.FavoriteScreen
+import me.algosketch.shopliveassignment.presentation.search.SearchScreen
+
+@Composable
+fun SlNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+
+    NavHost(
+        navController = navController,
+        startDestination = Search.route,
+    ) {
+        composable(route = Search.route) {
+            SearchScreen()
+        }
+        composable(route = Favorite.route) {
+            FavoriteScreen()
+        }
+    }
+}
