a0271dd6443e2cb9749757120a86d28b2ba723e6
algosketch <sjy_9484@naver.com>
2023-06-23 17:11:00 +0900

    Feat: UI에서 사용할 데이터 모델 정의

---
+++ app/src/main/java/me/algosketch/shopliveassignment/presentation/search/CharacterEntity.kt
@@ -0,0 +1,20 @@
+package me.algosketch.shopliveassignment.presentation.search
+
+import me.algosketch.shopliveassignment.data.source.remote.Character
+
+data class CharacterEntity(
+    val name: String,
+    val description: String,
+    val thumbnailUrl: String,
+)
+
+fun Character.toEntity(): CharacterEntity {
+    val imageSize = "standard_xlarge"
+    val prefixUrl = thumbnail.path.substring(4)
+
+    return CharacterEntity(
+        name = name,
+        description = description,
+        thumbnailUrl = "https${prefixUrl}/$imageSize.${thumbnail.extension}",
+    )
+}
