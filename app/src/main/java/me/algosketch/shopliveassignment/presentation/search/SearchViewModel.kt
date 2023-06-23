a36ff2854b7162c8a5fb9db736a135fada65aa11
algosketch <sjy_9484@naver.com>
2023-06-23 17:11:08 +0900

    Feat: [SearchViewModel] state 정의, 기본 search 함수 구현

--- app/src/main/java/me/algosketch/shopliveassignment/presentation/search/SearchViewModel.kt
+++ app/src/main/java/me/algosketch/shopliveassignment/presentation/search/SearchViewModel.kt
@@ -3,13 +3,50 @@ package me.algosketch.shopliveassignment.presentation.search
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import me.algosketch.shopliveassignment.data.repository.MarvelCharacterRepository
+import me.algosketch.shopliveassignment.data.source.ApiResponse
 import javax.inject.Inject
+
+sealed class SearchUiState {
+    object Loading : SearchUiState()
+    data class Success(
+        val characters: List<CharacterEntity> = emptyList(),
+    ) : SearchUiState()
+    data class Error(val message: String) : SearchUiState()
+}
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     private val searchRepository: MarvelCharacterRepository
 ) : ViewModel() {
+    private val _keyword = MutableStateFlow("")
+    val keyword = _keyword.asStateFlow()
+    private val _state = MutableStateFlow<SearchUiState>(SearchUiState.Success())
+    val state = _state.asStateFlow()
 
+    fun search() {
+        viewModelScope.launch {
+            _state.value = SearchUiState.Loading
+
+            if(keyword.value.length < 2) return@launch
+
+            val res = searchRepository.getMarvelCharacters(keyword.value)
+
+            _state.value = when(res) {
+                is ApiResponse.Success -> {
+                    val list = res.data?.data!!.results.map { it.toEntity() }
+                    SearchUiState.Success(list)
+                }
+                is ApiResponse.Error -> SearchUiState.Error(res.message)
+                else -> SearchUiState.Success(emptyList())
+            }
+        }
+    }
+
+    fun updateKeyword(newKeyword: String) {
+        _keyword.value = newKeyword
+    }
 }
