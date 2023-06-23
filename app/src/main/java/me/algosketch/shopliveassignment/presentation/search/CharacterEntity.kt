cf53428374a31073b737eb4489aaa207225da580
algosketch <sjy_9484@naver.com>
2023-06-23 19:58:56 +0900

    Feat: 데이터 변환 함수 추가

--- app/src/main/java/me/algosketch/shopliveassignment/presentation/search/CharacterEntity.kt
+++ app/src/main/java/me/algosketch/shopliveassignment/presentation/search/CharacterEntity.kt
@@ -1,19 +1,31 @@
 package me.algosketch.shopliveassignment.presentation.search
 
+import me.algosketch.shopliveassignment.data.source.local.FavoriteCharacter
 import me.algosketch.shopliveassignment.data.source.remote.Character
 
 data class CharacterEntity(
+    val id: Int,
     val name: String,
     val description: String,
     val thumbnailUrl: String,
     val favorite: Boolean = false,
-)
+) {
+    fun toFavoriteCharacter(): FavoriteCharacter {
+        return FavoriteCharacter(
+            characterId = id,
+            name = name,
+            description = description,
+            thumbnailUrl = thumbnailUrl,
+        )
+    }
+}
 
 fun Character.toEntity(): CharacterEntity {
     val imageSize = "standard_xlarge"
     val prefixUrl = thumbnail.path.substring(4)
 
     return CharacterEntity(
+        id = id,
         name = name,
         description = description,
         thumbnailUrl = "https${prefixUrl}/$imageSize.${thumbnail.extension}",
