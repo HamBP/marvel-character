a07e61cb56ba1649053ccb877bec0f97b19bf065
algosketch <sjy_9484@naver.com>
2023-06-23 21:40:01 +0900

    Feat: 바텀 네비게이션과 화면 연결

--- app/src/main/java/me/algosketch/shopliveassignment/presentation/MainActivity.kt
+++ app/src/main/java/me/algosketch/shopliveassignment/presentation/MainActivity.kt
@@ -3,15 +3,24 @@ package me.algosketch.shopliveassignment.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import me.algosketch.shopliveassignment.presentation.search.SearchScreen
+import me.algosketch.shopliveassignment.presentation.navigation.BottomNavDestination
+import me.algosketch.shopliveassignment.presentation.navigation.Search
+import me.algosketch.shopliveassignment.presentation.navigation.SlNavHost
+import me.algosketch.shopliveassignment.presentation.navigation.bottomNavScreens
 import me.algosketch.shopliveassignment.presentation.ui.theme.ShopLiveAssignmentTheme
 
 @AndroidEntryPoint
@@ -20,13 +29,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShopLiveAssignmentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    SearchScreen()
-                }
+                SlApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun SlApp() {
+
+    val navController = rememberNavController()
+    val currentNavBackStack = navController.currentBackStackEntryAsState()
+    val currentDestination = currentNavBackStack.value?.destination
+    val currentScreen = bottomNavScreens.find { it.route == currentDestination?.route } ?: Search
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background,
+    ) {
+        Scaffold(
+            bottomBar = { SlBottomNav(
+                allScreens = bottomNavScreens,
+                onTabSelected = { destination ->
+                    navController.navigate(destination.route)
+                },
+                currentScreen = currentScreen
+            ) }
+        ) { innerPadding ->
+            SlNavHost(
+                navController = navController,
+                modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
+
+@Composable
+fun SlBottomNav(
+    allScreens: List<BottomNavDestination>,
+    onTabSelected: (BottomNavDestination) -> Unit,
+    currentScreen: BottomNavDestination,
+) {
+    Row {
+        allScreens.forEach { screen ->
+            val selected = currentScreen == screen
+
+            Box(
+                modifier = Modifier
+                .weight(1f, true)
+                .height(48.dp)
+                .clickable { onTabSelected(screen) },
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    screen.route,
+                    color = if(selected) Color.Blue else Color.Black
+                )
             }
         }
     }
