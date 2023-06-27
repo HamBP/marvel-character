7fa8fdb4877f93b964b2a1a852e5d6adb85fc005
algosketch <sjy_9484@naver.com>
2023-06-28 01:47:20 +0900

    Refactor: 사용하지 않은 field 삭제

--- app/src/main/java/me/algosketch/shopliveassignment/data/repository/MarvelCharacterRepository.kt
+++ app/src/main/java/me/algosketch/shopliveassignment/data/repository/MarvelCharacterRepository.kt
@@ -12,20 +12,17 @@ class MarvelCharacterRepository @Inject constructor(
 ) {
     private val size: Int = 10
     private var page: Int = 1
-    private var total: Int = -1
     private var query: String = ""
         set(value) {
             if(query == value) return
 
             field = value
             page = 1
-            total = -1
         }
 
     suspend fun getMarvelCharacters(keyword: String): ApiResponse<CharacterDataWrapper> {
         query = keyword
         val res = source.load(query, size, page)
-        total = res.data?.data?.total ?: 0
         page++
 
         return res
